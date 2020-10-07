ace85cfdb293f77591087647a9d62649f9e1a916
ramcj7742 <ramcj7742@wrdsb.ca>
2020-10-07 13:37:33 +0000

    Finished final

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * Prints "black, white" amount of times as user number
+ * @author Jaden Ramcharan
  */
 public class Main {
 
@@ -9,7 +10,28 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // creates scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    //Gets number from user
+    System.out.println("Please enter the length of the line:");
+    int userNum = input.nextInt();
+
+    //loop that repeat prints black and white
+    for(int i = 1; i < userNum; i++){
+
+      //detects if count number is even or odd
+      if(i % 2 != 0){
+        System.out.print("black, ");
+      }else System.out.print("white, ");
+
+      //prints last word without comma at the end
+      if(i == userNum - 1 && userNum % 2 == 0){
+        System.out.print("white");
+      }else if(i == userNum - 1){
+      System.out.print("black");
+      }
+    }
     
   }
 }
